636ff24368fd094c72c0b4f57e53cab41c561959
pieter <pieter@masereeuw.nl>
2018-03-11 18:13:41 +0100

    verbeterd zipgedrag

--- software/sh/docx2jats_server_process.sh
+++ software/sh/docx2jats_server_process.sh
@@ -18,9 +18,12 @@ do
     mv "$f" "$parentdir/process"
     logfile=$parentdir/results/$basenamenoext.log
     echo "Start of docx2jats conversion of $basename on `date`" >$logfile 
-    $WHEREAMI/docx2jats.sh "$parentdir/process/$basename" >>"$logfile" 2>&1 
+    $WHEREAMI/docx2jats.sh "$parentdir/process/$basename" >>"$logfile" 2>&1
+    SAVEDIR=`pwd`
+    cd "$parentdir/process/$basenamenoext"
+    zip -r "$parentdir/results/$basenamenoext.zip" *
+    cd "$SAVEDIR"
     echo "End of docx2jats conversion of $basename on `date`" >>$logfile 
-    zip -r "$parentdir/results/$basenamenoext.zip" "$parentdir/process/$basenamenoext"
     rm -Rf "$parentdir/process/$basename" "$parentdir/process/$basenamenoext"
 done
 
